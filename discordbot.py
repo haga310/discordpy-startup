19228652cf404d30a45e50a21a3dda1af174af9d
haga310 <63287325+haga310@users.noreply.github.com>
2020-04-17 17:21:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def InsiderAdd(ctx):
     await ctx.send(f"{ctx.author.mention}さん")
-    await InMember.append(ctx.author.mention)
+    await InMember.append(str(ctx.author.mention))
     await ctx.send("log")
     await ctx.send(InMember)
     
