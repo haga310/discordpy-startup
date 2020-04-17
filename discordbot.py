eca8368b911a3c1bd60ee3f36dc7568440878051
haga310 <63287325+haga310@users.noreply.github.com>
2020-04-17 18:22:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
+@bot.command()
+async def InsiderAdd(ctx):
+    await ctx.send(f"{ctx.author.mention}さんをインサイダーゲームのプレイヤーに追加しました。")
+    InMember.append(str(ctx.author.mention))
 
 @bot.command()
 async def ping(ctx):
